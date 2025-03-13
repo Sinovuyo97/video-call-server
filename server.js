1243b89975666c48950cd0ddd86768e77ae8640b
SinoShakes <85928046+Sinovuyo97@users.noreply.github.com>
2025-03-13 20:09:06 +0200

    multiple

--- server.js
+++ server.js
@@ -13,26 +13,36 @@ const io = socketIo(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
 
-  socket.on("offer", (data) => {
-    socket.broadcast.emit("offer", data);
+  socket.on('join-call', () => {
+    // Notify all users about the new user
+    socket.broadcast.emit('user-joined', socket.id);
+
+    // Send the list of all users to the new user
+    const users = Array.from(io.sockets.sockets.keys()).filter(id => id !== socket.id);
+    socket.emit('all-users', users);
   });
 
-  socket.on("answer", (data) => {
-    socket.broadcast.emit("answer", data);
+  socket.on('offer', (data) => {
+    io.to(data.to).emit('offer', { from: socket.id, offer: data.offer });
   });
 
-  socket.on("ice-candidate", (data) => {
-    socket.broadcast.emit("ice-candidate", data);
+  socket.on('answer', (data) => {
+    io.to(data.to).emit('answer', { from: socket.id, answer: data.answer });
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  socket.on('ice-candidate', (data) => {
+    io.to(data.to).emit('ice-candidate', { from: socket.id, candidate: data.candidate });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    socket.broadcast.emit('user-disconnected', socket.id);
   });
 });
 
-server.listen(5000, '0.0.0.0', () => {
-  console.log("Server running on port 5000");
+server.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on http://0.0.0.0:3000");
 });
